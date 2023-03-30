63ac2d4ef000e0f9fee1190edd01c7f62c91f531
abdulrahmansamy <abdul.m.samy@gmail.com>
2023-03-30 08:54:16 +0300

    Update bash scripts

--- 04- LINUX/check_linux_version.sh
+++ 04- LINUX/check_linux_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $(grep -E '^(ID|NAME)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '"') == "Ubuntu" ]]; then
+if [[ $(grep -E '^(ID|NAME)="?' /etc/os-release &> /dev/null | awk -F= '{print $2}' | tr -d '"') == "Ubuntu" ]]; then
     if [[ $(grep -E '^(VERSION_ID)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '"') == "20.04" ]]; then
         echo "Running Ubuntu 20.04 LTS"
         # Run command for Ubuntu 20.04 LTS
@@ -10,7 +10,7 @@ if [[ $(grep -E '^(ID|NAME)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '
     else
         echo "Unsupported Ubuntu version"
     fi
-elif [[ $(grep -E '^(ID|NAME)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '"') == "CentOS Linux" ]]; then
+elif [[ $(grep -E '^(ID|NAME)="?' /etc/os-release &> /dev/null | awk -F= '{print $2}' | tr -d '"') == "CentOS Linux" ]]; then
     if [[ $(grep -E '^(VERSION_ID)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '"' | cut -c1) == "7" ]]; then
         echo "Running CentOS 7"
         # Run command for CentOS 7
@@ -20,15 +20,26 @@ elif [[ $(grep -E '^(ID|NAME)="?' /etc/os-release | awk -F= '{print $2}' | tr -d
     else
         echo "Unsupported CentOS version"
     fi
-elif [[ $(grep -E '^(NAME)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '"') == "Fedora Linux" ]]; then
+elif [[ $(grep -E '^(NAME)="?' /etc/os-release &> /dev/null | awk -F= '{print $2}' | tr -d '"') == "Fedora Linux" ]]; then
     if [[ $(grep -E '^(VERSION_ID)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '"') == "36" ]]; then
         echo "Running Fedora 36"
-        # Run command for CentOS 7
+        # Run command for Fedora 36
     elif [[ $(grep -E '^(VERSION_ID)="?' /etc/os-release | awk -F= '{print $2}' | tr -d '"') == "37" ]]; then
         echo "Running Fedora 37"
-        # Run command for CentOS 8
+        # Run command for Fedora 37
     else
-        echo "Unsupported CentOS version"
+        echo "Unsupported Fedora version"
+    fi
+elif [[ $(system_profiler SPSoftwareDataType | grep -i 'system version' | awk -F' ' '{print $3}') == "macOS" ]]; then
+    if [[ $(system_profiler SPSoftwareDataType | grep -i 'system version' | awk -F' ' '{print $4}') == "13.2.1" ]]; then
+        echo "Running macOS 13.2.1"
+        # Run command for macOS 13.2.1
+    elif [[ $(system_profiler SPSoftwareDataType | grep -i 'system version' | awk -F' ' '{print $4}') == "13.2.2" ]]; then
+        echo "Running macOS 13.2.2"
+        # Run command for macOS 13.2.2
+    
+    else
+        echo "Unsupported macOS version"
     fi
 else
     echo "Unsupported Linux distribution"
