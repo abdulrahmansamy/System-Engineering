48d5c5414ac995761a84ade2df45d6ab88645dd8
abdulrahmansamy <abdul.m.samy@gmail.com>
2024-03-08 00:01:19 +0300

    update harbor

--- 07-Harbor/03-configure_harbor-yaml.sh
+++ 07-Harbor/03-configure_harbor-yaml.sh
@@ -5,6 +5,10 @@ export YOURDOMAIN=$(hostname)
 
 cp ~/harbor/online-installer/harbor/harbor.yml.tmpl ~/harbor/online-installer/harbor/harbor.yml
 
+echo 
+echo -e "\033[1mConfigure Harbor Yaml file\033[0m"
+echo -e "\033[1m==========================\033[0m"
+
 sed -i "s|reg.mydomain.com|$YOURDOMAIN|g" ~/harbor/online-installer/harbor/harbor.yml
 #sed -i -e "s|\/your\/certificate\/path|$CERTIFICATE_DIR\/$YOURDOMAIN.crt|g" ~/harbor/online-installer/harbor/harbor.yml
 #sed -i -e "s/\/your\/private\/key\/path/$CERTIFICATE_DIR\/$YOURDOMAIN.key/g" ~/harbor/online-installer/harbor/harbor.yml
