d31f0171e2b83d4f6d565ce73584011d100e044b
abdulrahmansamy <abdul.m.samy@gmail.com>
2024-03-08 14:17:21 +0300

    Adding docker installation

--- 07-Harbor/00-prerequisites.sh
+++ 07-Harbor/00-prerequisites.sh
@@ -37,6 +37,42 @@ pip3 install podman-compose
 
 podman-compose --version
 
+echo 
+echo -e "\033[1mRemoving old versions of docker\033[0m"
+echo -e "\033[1m===============================\033[0m"
+
+
+
+sudo dnf remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+echo 
+echo -e "\033[1mInstalling docker\033[0m"
+echo -e "\033[1m=================\033[0m"
+
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo 
+echo -e "\033[1mInstalling docker compose\033[0m"
+echo -e "\033[1m=========================\033[0m"
+sudo dnf install -y docker-compose
+
+sudo systemctl start docker
+
+docker --version
+docker-compose --version
+
 echo
 echo -e "\033[1mAllowing 443, 4443 and 80 ports and HTTPS,HTTPS protocols\033[0m"
 echo -e "\033[1m=========================================================\033[0m"
